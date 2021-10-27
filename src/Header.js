3db52a3b0bf92f96af315a5129dd700dbc025551
Bostjan <bostjanzup@gmail.com>
2021-10-27 16:37:28 +0200

    pixel prefrect header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import sign from './assets/logoTimer.svg';
 
+const HeaderTag = styled.header`
+    padding-left: 80px;
+    padding-right: 80px;
+    height: 100px;
+  `;
+
+const ImageWrapper = styled.a`
+    float: left;
+    margin-top: 32px;
+`;
+
+const NavLink = styled(Link)`
+  float: right;
+  margin-left: 25.5px;
+  font-size: 16px;
+  line-height: 102px;
+  text-decoration: none;
+`;
 function Header() {
   return (
-    <header>
-      Header Placeholder
-    </header>
+    <HeaderTag>
+      <ImageWrapper href="/">
+        <img src={sign} alt="logo" />
+      </ImageWrapper>
+      <NavLink to="/">
+        About
+      </NavLink>
+      <NavLink to="/">
+        How it works
+      </NavLink>
+      <NavLink to="/search">
+        Search
+      </NavLink>
+    </HeaderTag>
   );
 }
 
