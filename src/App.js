379fae16d036913b492450a8855a40673e2019ab
Bostjan <bostjanzup@gmail.com>
2021-10-27 11:00:44 +0200

    added normalize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './assets/normalize.css';
 import './index.css';
 
 function App() {
   return (
     <div>
+      <h2>gffsd</h2>
       App Placeholder
+      <button type="button">Kaj</button>
     </div>
   );
 }
