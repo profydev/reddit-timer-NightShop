5bfb76435876906d6c91aa0dfbfe941b8787a181
Bostjan <bostjanzup@gmail.com>
2021-10-27 11:23:21 +0200

    added router and styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './assets/normalize.css';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h2>gffsd</h2>
-      App Placeholder
-      <button type="button">Kaj</button>
+      <Router>
+        <Switch>
+          <Route path="/">
+
+          </Route>
+          <Route path="/search">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
